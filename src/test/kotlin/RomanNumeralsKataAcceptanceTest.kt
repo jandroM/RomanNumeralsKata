5ddb3246dfd480c90438c6c160b2cf4289087c99
Karla Astrid Quispe Ulloa <karla.quispe@wallapop.com>
2021-10-21 16:24:02 +0200

    First commit

--- src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
+++ src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
@@ -1,9 +1,14 @@
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class RomanNumeralsKataAcceptanceTest {
 
     @Test
     fun `it should do whatever`(){
+        assertEquals("I", convert(1))
+    }
 
+    private fun convert(number: Int): String {
+        return "I"
     }
 }
