ff0b2c0fd0eec76fefb88a65d8f7c7dc5ecef579
Bartomeu Galmés <bartomeu.galmes@wallapop.com>
2021-10-21 17:32:35 +0200

    added parametrized test

--- src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
+++ src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
@@ -1,82 +1,101 @@
-import org.junit.Assert
-import org.junit.Test
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class RomanNumeralsKataAcceptanceTest {
 
     @Test
     fun `it should convert 1 to roman numeral`(){
-        Assert.assertEquals(convert(1), "I")
+        assertEquals(convert(1), "I")
     }
 
     @Test
     fun `it should convert 2 to roman numeral`(){
-        Assert.assertEquals(convert(2), "II")
+        assertEquals(convert(2), "II")
     }
 
     @Test
     fun `it should convert 3 to roman numeral`(){
-        Assert.assertEquals(convert(3), "III")
+        assertEquals(convert(3), "III")
     }
 
     @Test
     fun `it should convert 4 to roman numeral`(){
-        Assert.assertEquals(convert(4), "IV")
+        assertEquals(convert(4), "IV")
     }
 
     @Test
     fun `it should convert 14 to roman numeral`(){
-        Assert.assertEquals(convert(14), "XIV")
+        assertEquals(convert(14), "XIV")
     }
 
     @Test
     fun `it should convert 29 to roman numeral`(){
-        Assert.assertEquals(convert(29), "XXIX")
+        assertEquals(convert(29), "XXIX")
     }
 
     @Test
     fun `it should convert 54 to roman numeral`(){
-        Assert.assertEquals(convert(54), "LIV")
+        assertEquals(convert(54), "LIV")
     }
 
     @Test
     fun `it should convert 99 to roman numeral`(){
-        Assert.assertEquals(convert(99), "IC")
+        assertEquals(convert(99), "IC")
     }
 
     @Test
     fun `it should convert 101 to roman numeral`(){
-        Assert.assertEquals(convert(101), "CI")
+        assertEquals(convert(101), "CI")
     }
 
     @Test
     fun `it should convert 199 to roman numeral`(){
-        Assert.assertEquals(convert(199), "CIC")
+        assertEquals(convert(199), "CIC")
     }
 
     @Test
     fun `it should convert 523 to roman numeral`(){
-        Assert.assertEquals(convert(523), "DXXIII")
+        assertEquals(convert(523), "DXXIII")
     }
 
     @Test
     fun `it should convert 1719 to roman numeral`(){
-        Assert.assertEquals("MDCCXIX", convert(1719))
+        assertEquals("MDCCXIX", convert(1719))
     }
 
     @Test
     fun `it should convert 3724 to roman numeral`(){
-        Assert.assertEquals("MMMDCCXXIV", convert(3724))
+        assertEquals("MMMDCCXXIV", convert(3724))
     }
 
     @Test
     fun `it should convert 79 to roman numeral`(){
-        Assert.assertEquals("LXXIX", convert(79))
+        assertEquals("LXXIX", convert(79))
     }
 
     @Test
     fun `it should convert 904 to roman numeral`(){
-        Assert.assertEquals("CMIV", convert(904))
+        assertEquals("CMIV", convert(904))
     }
+
+    @ParameterizedTest
+    @CsvSource("CMIV,904",
+        "DXXV,525",
+        "XXVII,27",
+        "LIII,53",
+        "DLXVII,567",
+        "CLXXIX,179",
+        "CV,105",
+        "CMXVI,916",
+        "DXCVI,596",
+        "XXXVI,36")
+    internal fun name(roman:String,number:Int) {
+        assertEquals(roman, convert(number))
+    }
+
     private fun convert(number: Int): String {
         var result  = ""
         var tempNumber = number
@@ -86,6 +105,7 @@ class RomanNumeralsKataAcceptanceTest {
             500 to "D",
             100 to "C",
             99 to "IC",
+            90 to "XC",
             50 to "L",
             10 to "X",
             9 to "IX",
