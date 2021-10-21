6dbcf3d711a67cffbe2365dc307dd85a113ee10b
Alejandro <alejandro.miralles@wallapop.com>
2021-10-21 17:07:12 +0200

    Refactor in convert function

--- src/main/kotlin/RomanNumeralsKata.kt
+++ src/main/kotlin/RomanNumeralsKata.kt
@@ -1,18 +1,15 @@
 open class RomanNumeralsKata {
     fun convert(number: Int): String {
-        if( number == 4) return "IV"
-        if( number == 5) return "V"
-        if( number == 6) return "VI"
         return printRomanNumber(number)
     }
 
     private fun printRomanNumber(times: Int): String {
         var returnValue = ""
         for(i in 1..times){
-            if ( i == 5){
-                returnValue = "V"
-            } else {
-                returnValue += "I"
+            when (i) {
+                5 -> returnValue = "V"
+                4 -> returnValue = "IV"
+                else ->returnValue += "I"
             }
         }
         return returnValue
