a329c45e13acd8f6e10a7f606e9abd46c63990c0
Bartomeu Galmés <bartomeu.galmes@wallapop.com>
2021-10-21 16:40:29 +0200

    Converts 14

--- src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
+++ src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
@@ -23,8 +23,23 @@ class RomanNumeralsKataAcceptanceTest {
         Assert.assertEquals(convert(4), "IV")
     }
 
+    @Test
+    fun `it should convert 14 to roman numeral`(){
+        Assert.assertEquals(convert(14), "XIV")
+    }
+
     private fun convert(number: Int): String {
-        val numbers = mapOf(1 to "I", 2 to "II", 3 to "III", 4 to "IV")
-        return numbers[number]!!
+        var result  = ""
+        var tempNumber = number
+        val numbers = mapOf(10 to "X", 5 to "V", 4 to "IV",3 to "III", 2 to "II", 1 to "I")
+        while (tempNumber > 0){
+            numbers.forEach{(k,v)->
+                if(tempNumber >= k){
+                    result += v
+                    tempNumber -= k
+                }
+            }
+        }
+        return result
     }
 }
