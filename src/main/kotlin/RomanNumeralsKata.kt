8fe15f743d2341bc78d6e260e9b2408a123c283d
Karla Astrid Quispe Ulloa <karla.quispe@wallapop.com>
2021-10-21 16:34:20 +0200

    Extract function to class

---
+++ src/main/kotlin/RomanNumeralsKata.kt
@@ -0,0 +1,6 @@
+open class RomanNumeralsKata {
+    fun convert(number: Int): String {
+        if (number == 1) return "I"
+        else return "II"
+    }
+}
--- src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
+++ src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
@@ -1,16 +1,11 @@
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class RomanNumeralsKataAcceptanceTest {
+class RomanNumeralsKataAcceptanceTest : RomanNumeralsKata() {
 
     @Test
     fun `it should do whatever`(){
         assertEquals("I", convert(1))
         assertEquals("II", convert(2))
-    }
-
-    private fun convert(number: Int): String {
-        if (number == 1) return "I"
-        else return "II"
     }
 }
