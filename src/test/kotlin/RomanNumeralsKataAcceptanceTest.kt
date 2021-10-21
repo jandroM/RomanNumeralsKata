193d95c6f370bc24b4525d79ece2d0ca54ea0bc6
Alejandro <alejandro.miralles@wallapop.com>
2021-10-21 17:00:13 +0200

    Tests splitted by numbers

--- src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
+++ src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
@@ -4,11 +4,23 @@ import kotlin.test.assertEquals
 class RomanNumeralsKataAcceptanceTest : RomanNumeralsKata() {
 
     @Test
-    fun `it should convert an integer number to their roman numeral interpretation`(){
-        assertEquals("I", convert(1))
-        assertEquals("II", convert(2))
-        assertEquals("III", convert(3))
-        assertEquals("IV", convert(4))
+    fun `it should convert 5 to the roman numeral interpretaion`(){
         assertEquals("V", convert(5))
+    }
+    @Test
+    fun `it should convert 4 to the roman numeral interpretaion`(){
+        assertEquals("IV", convert(4))
+    }
+    @Test
+    fun `it should convert 3 to the roman numeral interpretaion`(){
+        assertEquals("III", convert(3))
+    }
+    @Test
+    fun `it should convert 2 to the roman numeral interpretaion`(){
+        assertEquals("II", convert(2))
+    }
+    @Test
+    fun `it should convert 1 to the roman numeral interpretaion`(){
+        assertEquals("I", convert(1))
     }
 }
