ad28086073a3fec646399490fb7955843ef5c025
Bartomeu Galmés <bartomeu.galmes@wallapop.com>
2021-10-21 16:18:23 +0200

    Converts 1

--- src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
+++ src/test/kotlin/RomanNumeralsKataAcceptanceTest.kt
@@ -1,9 +1,14 @@
+import org.junit.Assert
 import org.junit.Test
 
 class RomanNumeralsKataAcceptanceTest {
 
     @Test
-    fun `it should do whatever`(){
+    fun `it should convert 1 to roman numeral`(){
+        Assert.assertEquals(convert(1), "I")
+    }
 
+    private fun convert(number: Int): String {
+        return "I"
     }
 }
